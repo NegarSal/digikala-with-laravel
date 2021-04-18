d0895cabd2cfd4bc5aba62bded8e2a64185eb856
NegarSal <Negarsaljooghi@gmail.com>
2021-04-18 17:53:12 +0430

    second commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.frontend')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+    <h1 class="mt-6">Hello</h1>
+@endsection
+
+
---
+++ resources/views/layouts/inc/front-navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="py-2 fixed-top bg-light">
+  <div class="container d-flex flex-wrap">
+    <ul class="nav me-auto">
+      <li class="nav-item">
+        <form class="navbar-form navbar-left" action="/action_page.php">
+          <div class="input-group">
+            <div class="input-group-btn">
+               <a href="#"> <i class='fas fa-cart-plus' style='font-size:15px'></i></a>
+            </div>
+            <input type="text" class="form-control" placeholder="ورود به حساب کاربري" name="search">
+          </div>
+        </form>
+      </li>
+    </ul>
+      <form class="col col-lg-8 container-fluid">
+        <div class="input-group">
+          <span class="input-group-text" id="basic-addon1"><i class="fa fa-fw fa-search"></i>
+            <input type="search" class="form-control " placeholder="جستجو در دیجی کالا...">
+          </span>
+        </div>
+      </form>
+        <a class="navbar-brand" href="#"><b class="text-danger">digikala</b></a>
+  </div>
+  <header class="py-3 border-bottom">
+    <div class="container d-flex flex-wrap">
+      <a href="#" class="d-flex me-auto text-dark text-decoration-none">
+        <form class="col-12 col-lg-auto mb-lg-0">
+          <input type="search" class="form-control" placeholder="لطفا شهر و استان خود را انتخاب کنید">
+        </form>
+      </a>
+      <ul class="nav">
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3">فروشنده شوید</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3">سوالی دارید؟</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3"> دیجی کلاب <i class="fa fa-crosshairs"></i> </a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3">دیجی پلاس <i class="fa fa-crosshairs"></i> </a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3"> دیجی کالای من <i class="fas fa-medal"></i> </a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3">تخفیف ها وپیشنهاد <i class="fa fa-percent"></i> </a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3">سوپرمارکت <i class="fa fa-shopping-basket"></i> </a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-3">دسته بندی کالاها <i class="fas fa-bars"></i> </a></li>
+      </ul>
+    </div>
+  </header>
+</nav>
