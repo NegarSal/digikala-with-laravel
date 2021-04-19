bd3b7b0652cbebfb54bc0cea463282d3c187cfcd
NegarSal <Negarsaljooghi@gmail.com>
2021-04-19 15:38:32 +0430

    adding footer

---
+++ resources/views/layouts/inc/front-footer.blade.php
@@ -0,0 +1,148 @@
+<!-- Footer -->
+<footer class="bg-light text-center text-secondary container-fluid ">
+    <div class="container p-4  my-3">
+      <!--Footer P1-->
+     <a href="#" class="up">برگشت به بالا <img src="./img/up.png" height="30" width="30"></a>
+      <hr>
+  <!--END Footer P1-->
+
+  <!--Footer P2-->
+      <section>
+        <div class="container">
+            <div class="row my-3">
+            </div>
+            <div class="row">
+                <div class="col-lg-2 col-sm-6">
+                    <a href="#"><img src="./img/f-1.png" class="img-thumbnail"></a>
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                  <a href="#"><img src="./img/f-2.png" class="img-thumbnail"></a>
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                  <a href="#"><img src="./img/f-3.png" class="img-thumbnail"></a>
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                  <a href="#"><img src="./img/f-4.png" class="img-thumbnail"></a>
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                  <a href="#"><img src="./img/f-5.png" class="img-thumbnail"></a>
+                </div>
+            </div>
+        </div>
+      </section>
+  <!--END Footer P2-->
+      <hr>
+  <!--Footer P3-->
+      <section>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">از تخفیف‌ها و جدیدترین‌های دیجی‌کالا باخبر شوید</h5>
+            <ul class="list-unstyled mb-0">
+              <li>
+                <div class="row d-flex ">
+                <div class="form-outline form-white mb-4">
+                  <input type="email" id="form5Example2" class="form-control" placeholder="آدرس ایمیل خود را وارد کنید"/>
+                  <button type="submit" class="btn btn-primary mb-4">
+                    ارسال
+                  </button>
+                </div>
+              </div>
+              </li>
+              <li>
+                <a href="#!" class="text-secondary">دیجی‌کالا را در شبکه‌های اجتماعی دنبال کنید</a>
+              </li>
+              <li>
+                <section class="mb-4">
+                  <a class="btn btn-outline-secondary btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+                  <a class="btn btn-outline-secondary btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+                  <a class="btn btn-outline-secondary btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                </section>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5>با دیجی‌کالا</h5>
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-gray">اتاق خبر دیجی‌کالا</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">فروش در دیجی‌کالا</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">فرصت‌های شغلی</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">تماس با دیجی‌کالا</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">درباره دیجی‌کالا</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">راهنمای هویت بصری</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5>خدمات مشتریان</h5>
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-gray">پاسخ به پرسش‌های متداول</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">رویه‌های بازگرداندن کالا</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">شرایط استفاده</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">حریم خصوصی</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray"> گزارش باگ</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5>راهنمای خرید از دیجی‌کالا</h5>
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-gray">نحوه ثبت سفارش</a>
+              </li>
+              <li>
+                <a href="#!" class="text-gray">رویه ارسال سفارش</a>
+              </li>
+              <li>
+                <a href="#" class="text-gray">شیوه‌های پرداخت</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <!--END Footer P3-->
+      <hr>
+  <!--Footer P4-->
+      <section class="mb-4">
+        <p> هفت روز هفته ، ۲۴ ساعت شبانه‌روز پاسخگوی شما هستیم
+          شماره تماس :۶۱۹۳۰۰۰۰ - ۰۲۱
+         info@digikala.com :آدرس ایمیل
+        </p>
+        <div>
+          <button class="btn btn-dark mb-4">دریافت نسخهIOS</button>
+          <button class="btn btn-dark mb-4">دریافت از مایکت</button>
+          <button class="btn btn-dark mb-4">دریافت از بازار</button>
+          <button class="btn btn-dark mb-4"> GooglePlay </button>
+        </div>
+      </section>
+    </div>
+      <hr>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(247, 216, 216, 0.2);">
+      استفاده از مطالب فروشگاه اینترنتی دیجی‌کالا فقط برای مقاصد غیرتجاری و با ذکر منبع بلامانع است. کلیه حقوق این سایت متعلق به شرکت نوآوران فن آوازه (فروشگاه آنلاین دیجی‌کالا) می‌باشد.
+    </div>
+    <!--Footer P4-->
+  </footer>
+  <!-- Footer -->
