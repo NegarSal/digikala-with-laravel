955c16e51a1b07e531cc058f9d485e6cd3bef555
NegarSal <Negarsaljooghi@gmail.com>
2021-04-19 21:12:26 +0430

    adding modal for location

--- resources/views/layouts/inc/front-navbar.blade.php
+++ resources/views/layouts/inc/front-navbar.blade.php
@@ -4,9 +4,9 @@
       <li class="nav-item">
         <form class="navbar-form navbar-left" action="/action_page.php">
           <div class="input-group">
-            <div class="input-group-btn">
-               <a href="#"> <i class='fas fa-cart-plus' style='font-size:15px'></i></a>
-            </div>
+              <div class="input-group-btn">
+                 <a href="#"> <i class='fas fa-cart-plus' style='font-size:15px'></i></a>
+               </div>
             <a href="login"><input type="text" class="form-control" placeholder="ورود به حساب کاربري" name="search"></a>
           </div>
         </form>
@@ -23,11 +23,7 @@
   </div>
   <header class="py-3 border-bottom">
     <div class="container d-flex ">
-      <a href="#" class="d-flex me-auto text-dark text-decoration-none">
-        <form class="col-12 col-lg-auto mb-lg-0">
-          <input type="search" class="form-control" placeholder="لطفا شهر و استان خود را انتخاب کنید">
-        </form>
-      </a>
+
       <ul class="nav">
         <li class="nav-item"><a href="#" class="nav-link link-dark px-3">فروشنده شوید</a></li>
         <li class="nav-item"><a href="#" class="nav-link link-dark px-3">سوالی دارید؟</a></li>
@@ -38,9 +34,66 @@
         <li class="nav-item"><a href="#" class="nav-link link-dark px-3">سوپرمارکت <i class="fa fa-shopping-basket"></i> </a></li>
         <li class="nav-item"><a href="#" class="nav-link link-dark px-3">دسته بندی کالاها <i class="fas fa-bars"></i> </a></li>
       </ul>
+
+<button type="button" class="btn btn-outline-secondary bg-white btn-lg" data-toggle="modal" data-target="#myModal">لطفا شهر و استان خود را انتخاب کنید</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" role="dialog">
+  <div class="modal-dialog modal-dialog-scrollable">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">انتخاب استان</h4>
+      </div>
+      <div class="modal-body" dir="rtl">
+        <p class="text-primary"><i class="fa fa-crosshairs"></i> مکان یابی خودکار</p><hr>
+        <p >آذربایجان شرقی</p><hr>
+        <p>آذربایجان غربی</p><hr>
+        <p>اردبیل</p><hr>
+        <p>اصفهان </p><hr>
+        <p>البرز</p><hr>
+        <p>ایلام</p><hr>
+        <p>بوشهر</p><hr>
+        <p>تهران</p><hr>
+        <p>خراسان رضوی</p><hr>
+        <p>خراسان جنوبی</p><hr>
+        <p>خراسان شمالی</p><hr>
+        <p>خوزستان</p><hr>
+        <p>زنجان</p><hr>
+        <p> سمنان</p><hr>
+        <p>سیستان و بلوچستان</p><hr>
+        <p>فارس</p><hr>
+        <p>قزوین</p><hr>
+        <p>قم</p><hr>
+        <p>لرستان</p><hr>
+        <p>مازندران</p><hr>
+        <p>مرکزی </p><hr>
+        <p>هرمزگان  </p><hr>
+        <p> همدان</p><hr>
+        <p>چهار محال و بختیاری</p><hr>
+        <p>کردستان</p><hr>
+        <p> کرمان</p><hr>
+        <p>کرمانشاه </p><hr>
+        <p>کهگیلویه و بویراحمد</p><hr>
+        <p>گلستان</p><hr>
+        <p> گیلان</p><hr>
+        <p>یزد </p>
+      </div>
+    </div>
+
+  </div>
+</div>
+</div>
     </div>
   </header>
 </nav>
 
 
 
+<style>
+ .modal-backdrop {
+    z-index: 0 !important;
+   }
+</style>
