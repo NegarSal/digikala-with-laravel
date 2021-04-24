31c87a2ff64f77165756cf74eceede54cb86f97b
NegarSal <Negarsaljooghi@gmail.com>
2021-04-24 18:05:54 +0430

    edit-part4:fresh-offers

--- resources/views/layouts/inc/middle.blade.php
+++ resources/views/layouts/inc/middle.blade.php
@@ -163,61 +163,157 @@
 </main>
 <!--End Part3-->
 
-  <!-- شگفت انگیز سوپرمارکتی-->
-  <section class="bg-success">
-    <div class="container my-4">
-      <div class="row"><h1>شگفت انگیز سوپرمارکتی</h1>
-        <img src="">
-        <div class="col-centered">
-          <div id="carousel" class="carousel slide" data-ride="carousel" data-type="multi" data-interval="2500">
-            <div class="carousel-inner">
-              <div class="item active">
-                <div class="carousel-col">
-                  <div><img src="{{ asset('img/4-1.png') }}"></div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="carousel-col">
-                  <div><img src="{{ asset('img/4-2.png') }}"></div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="carousel-col">
-                  <div><img src="{{ asset('img/4-3.png') }}"></div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="carousel-col">
-                  <div><img src="{{ asset('img/4-4.png') }}"></div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="carousel-col">
-                  <div><img src="{{ asset('img/4-5.png') }}"></div>
-                </div>
-              </div>
+<!--fresh-offers-->
+<main>
+    <article class="specialoffer marketSlide">
+        <section class="slider container flex">
+            <div class="right">
+                <a class="marketBanner" href=""></a>
+                <div class="showALL">مشاهده همه <i class='fas fa-angle-left'></i></div>
             </div>
-
-            <!-- Controls -->
-            <div class="left carousel-control">
-              <a href="#carousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
+            <!-- Swiper -->
+            <div class="swiper-container s1">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <a class="card"  href="">
+                    <div class="image">
+                        <img src="{{ asset('img/4.1.jpg') }}">
+                    </div>
+                    <div class="title">
+                        محصول شماره ۱
+                    </div>
+                    <div class="price">
+                        <div class="oldPrice">
+                          <del>۲۳۰۰۰</del>
+                          <span class="percent">۲۰٪</span>
+                        </div>
+                        <div class="newPrice">
+                            <span>۲۸۰۰۰</span>
+                            <span>تومان</span>
+                        </div>
+                    </div>
+                    <i class='far fa-clock'></i>
+                    <div class="timer" data-countdown="2021/09/10 21:28"></div>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                  <a class="card" href="">
+                    <div class="image">
+                        <img src="{{ asset('img/4.2.jpg') }}" alt="">
+                    </div>
+                    <div class="title">
+                        محصول شماره ۲
+                    </div>
+                    <div class="price">
+                        <div class="oldPrice">
+                          <del>۲۳۰۰۰</del>
+                          <span class="percent">۲۰٪</span>
+                        </div>
+                        <div class="newPrice">
+                            <span>۲۸۰۰۰</span>
+                            <span>تومان</span>
+                        </div>
+                    </div>
+                    <i class='far fa-clock'></i>
+                    <div class="timer" data-countdown="2021/10/14 22:22"></div>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                 <a class="card" href="">
+                    <div class="image">
+                        <img src="{{ asset('img/4.3.jpg') }}" alt="">
+                    </div>
+                    <div class="title">
+                        محصول شماره ۳
+                    </div>
+                    <div class="price">
+                        <div class="oldPrice">
+                          <del>۲۳۰۰۰</del>
+                          <span class="percent">۲۰٪</span>
+                        </div>
+                        <div class="newPrice">
+                            <span>۲۸۰۰۰</span>
+                            <span>تومان</span>
+                        </div>
+                    </div>
+                    <i class='far fa-clock'></i>
+                    <div class="timer" data-countdown="2021/08/15 00:00"></div>
+                 </a>
+                </div>
+                <div class="swiper-slide">
+                 <a class="card" href="">
+                    <div class="image">
+                        <img src="{{ asset('img/4.4.jpg') }}" alt="">
+                    </div>
+                    <div class="title">
+                        محصول شماره ۴
+                    </div>
+                    <div class="price">
+                        <div class="oldPrice">
+                          <del>۲۳۰۰۰</del>
+                          <span class="percent">۲۰٪</span>
+                        </div>
+                        <div class="newPrice">
+                            <span>۲۸۰۰۰</span>
+                            <span>تومان</span>
+                        </div>
+                    </div>
+                    <i class='far fa-clock'></i>
+                    <div class="timer" data-countdown="2021/07/19 18:40"></div>
+                 </a>
+                </div>
+                <div class="swiper-slide">
+                 <a class="card" href="">
+                    <div class="image">
+                        <img src="{{ asset('img/4.5.jpg') }}" alt="">
+                    </div>
+                    <div class="title">
+                        محصول شماره ۵
+                    </div>
+                    <div class="price">
+                        <div class="oldPrice">
+                          <del>۲۳۰۰۰</del>
+                          <span class="percent">۲۰٪</span>
+                        </div>
+                        <div class="newPrice">
+                            <span>۲۸۰۰۰</span>
+                            <span>تومان</span>
+                        </div>
+                    </div>
+                    <i class='far fa-clock'></i>
+                    <div class="timer" data-countdown="2021/11/01 17:03"></div>
+                 </a>
+                </div>
+                <div class="swiper-slide">
+                 <a class="card" href="">
+                    <div class="image">
+                        <img src="{{ asset('img/4.6.jpg') }}" alt="">
+                    </div>
+                    <div class="title">
+                        محصول شماره ۶
+                    </div>
+                    <div class="price">
+                        <div class="oldPrice">
+                          <del>۲۳۰۰۰</del>
+                          <span class="percent">۲۰٪</span>
+                        </div>
+                        <div class="newPrice">
+                            <span>۲۸۰۰۰</span>
+                            <span>تومان</span>
+                        </div>
+                    </div>
+                    <i class='far fa-clock'></i>
+                    <div class="timer" data-countdown="2021/03/01 16:37"></div>
+                 </a>
+                </div>
             </div>
-            <div class="right carousel-control">
-              <a href="#carousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
             </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- شگفت انگیز سوپرمارکتی-->
+        </section>
+    </article>
+</main>
+<!--fresh-offers-->
 
   <!--Part 5-->
   <div class="row align-items-md-stretch my-4">
@@ -312,9 +408,13 @@
     background-position: center;
     background-size: cover;
 }
-
-
-
+.specialoffer .slider .marketBanner{
+    background: url("img/fr-o.png");
+    height: 250px;
+    display: block;
+    background-position: center;
+    background-size: cover;
+}
  .up{
     text-decoration: none;
     color: #000;
