6c61a0abaf91205181d9579c0afdb440d4c29790
NegarSal <Negarsaljooghi@gmail.com>
2021-04-28 01:03:01 +0430

    edit-first page-footer2

--- resources/views/layouts/inc/front-footer.blade.php
+++ resources/views/layouts/inc/front-footer.blade.php
@@ -1,5 +1,49 @@
 <!--footer-->
 <footer>
     <a href="#" class="scrollTop flex"><i class="fas fa-caret-up"></i>برگشت به بالا</a>
+    <nav class="feature flex container">
+        <a href=""><img src="{{ asset('img/footer/item1.svg') }}" alt="">
+            <div class="itemTitle">تحویل اکسپرس</div>
+        </a>
+        <a href=""><img src="{{ asset('img/footer/item2.svg') }}" alt="">
+            <div class="itemTitle">پشتیبانی ۲۴ ساعته</div>
+        </a>
+        <a href=""><img src="{{ asset('img/footer/item3.svg') }}" alt="">
+            <div class="itemTitle">پرداخت در محل</div></a>
+        <a href=""><img src="{{ asset('img/footer/item4.svg') }}" alt="">
+            <div class="itemTitle">۷روز ضمانت بازگشت</div>
+        </a>
+        <a href=""><img src="{{ asset('img/footer/item5.svg') }}" alt="">
+            <div class="itemTitle">ضمانت اصل بودن کالا</div>
+        </a>
+    </nav>
+    <div class="middleBar">
+        <div class="right flex">
+            <nav>
+                <a href="">راهنمای خرید از دیجی‌کالا</a>
+                <a href="">نحوه ثبت سفارش</a>
+                <a href="">رویه ارسال سفارش</a>
+                <a href="">شیوه‌های پرداخت</a>
+            </nav>
+            <nav>
+                <a href="">خدمات مشتریان</a>
+                <a href="">پاسخ به پرسش‌های متداول</a>
+                <a href="">رویه‌های بازگرداندن کالا</a>
+                <a href="">شرایط استفاده</a>
+                <a href="">حریم خصوصی</a>
+                <a href="">گزارش باگ</a>
+            </nav>
+            <nav>
+                <a href="">با دیجی‌کالا</a>
+                <a href="">اتاق خبر دیجی‌کالا</a>
+                <a href="">فروش در دیجی‌کالا</a>
+                <a href="">فرصت‌های شغلی</a>
+                <a href="">تماس با دیجی‌کالا</a>
+                <a href="">درباره دیجی‌کالا</a>
+                <a href="">راهنمای هویت بصری</a>
+            </nav>
+        </div>
+        <div class="left"></div>
+    </div>
 </footer>
 
