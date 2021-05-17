78186eceef61c12ce71cd99beac27650fa178d5a
NegarSal <Negarsaljooghi@gmail.com>
2021-05-17 18:18:27 +0430

    responsive-slider-banner1

--- resources/views/sections/banner1.blade.php
+++ resources/views/sections/banner1.blade.php
@@ -2,11 +2,11 @@
     <!-- Order your soul. Reduce your wants. - Augustine -->
 
 <article class="banner1">
-    <div class="container flex">
-        <a href="#"><img src="{{ asset('img/3.1.jpg') }}"></a>
-        <a href="#"><img src="{{ asset('img/3.2.jpg') }}"></a>
-        <a href="#"><img src="{{ asset('img/3.3.jpg') }}"></a>
-        <a href="#"><img src="{{ asset('img/3.4.jpg') }}"></a>
+    <div class="row fle11">
+        <a href="#" class="col-xs-12 col-sm-6 col-md-6 col-lg-3 mt-3"><img src="{{ asset('img/3.1.jpg') }}"></a>
+        <a href="#" class="col-xs-12 col-sm-6 col-md-6 col-lg-3 mt-3"><img src="{{ asset('img/3.2.jpg') }}"></a>
+        <a href="#" class="col-xs-12 col-sm-6 col-md-6 col-lg-3 mt-3"><img src="{{ asset('img/3.3.jpg') }}"></a>
+        <a href="#" class="col-xs-12 col-sm-6 col-md-6 col-lg-3 mt-3"><img src="{{ asset('img/3.4.jpg') }}"></a>
     </div>
 </article>
 
